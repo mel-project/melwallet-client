d531ce72a09cbe727eb995cb620c477660499085
sadministrator <marcothewebdev@gmail.com>
2023-03-21 15:09:05 -0600

    Fixed "Error: " bug on help commands.

--- src/bin/melwallet-cli.rs
+++ src/bin/melwallet-cli.rs
@@ -77,7 +77,7 @@ fn main() -> http_types::Result<()> {
     smolscale::block_on(async move {
         let mut twriter = TabWriter::new(std::io::stderr());
         let mut command = Args::command();
-        let args = Args::try_parse()?;
+        let args = Args::parse();
         if let Args::GenerateAutocomplete = args {
             generate(Bash, &mut command, "melwallet-cli", &mut std::io::stdout());
         };
